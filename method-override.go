44dd30e1a28fcebbf74e699c6dfea497944e093d
Roshan Gade <roshangade@gmail.com>
2019-06-15 20:00:32 +0530

    Merged post body into query on POST to GET method override

--- method-override.go
+++ method-override.go
@@ -6,9 +6,12 @@
 package methodoverride
 
 import (
+	"encoding/json"
+	"net/url"
+	"reflect"
 	"strings"
 
-	"github.com/go-rs/rest-api-framework"
+	rest "github.com/go-rs/rest-api-framework"
 )
 
 var (
@@ -29,6 +32,40 @@ func isValidMethod(method string) bool {
 }
 
 /**
+ * Convert json to string
+ */
+func jsonToString(val interface{}) string {
+	b, _ := json.Marshal(val)
+	return string(b)
+}
+
+/**
+ * High-level merge of request json into query
+ */
+func mergeQuery(query url.Values, body map[string]interface{}) url.Values {
+	for key, val := range body {
+		str, ok := val.(string)
+		if ok {
+			query.Add(key, str)
+		} else {
+			if reflect.TypeOf(val).String() == "[]interface {}" {
+				for _, val := range val.([]interface{}) {
+					str, ok = val.(string)
+					if ok {
+						query.Add(key, str)
+					} else {
+						query.Add(key, jsonToString(val))
+					}
+				}
+			} else {
+				query.Add(key, jsonToString(val))
+			}
+		}
+	}
+	return query
+}
+
+/**
  * Method Override
  */
 func Load() rest.Handler {
@@ -36,6 +73,16 @@ func Load() rest.Handler {
 		method := strings.ToUpper(ctx.Request.Header.Get(header))
 		if method != "" && isValidMethod(method) {
 			ctx.Set("OriginalMethod", ctx.Request.Method)
+			if ctx.Request.Method == "POST" && method == "GET" {
+
+				contentType := strings.ToLower(ctx.Request.Header.Get("content-type"))
+
+				if contentType == "application/json" {
+					ctx.Query = mergeQuery(ctx.Query, ctx.Body)
+				} else if len(ctx.Request.Form) > 0 {
+					ctx.Query = ctx.Request.Form
+				}
+			}
 			ctx.Request.Method = method
 		}
 	}
